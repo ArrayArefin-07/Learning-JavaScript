5ea7879b20cbf947136533736142733be494446d
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-06-03 14:39:51 +0600

    create add and sub function and call them

--- function.js
+++ function.js
@@ -4,11 +4,34 @@
 
 // Creating function 
 
-function square(){
-    var num = 5;
+function square(num){
+    // var num = 5;
     var result = num * num;
-    document.write("Result =" + result + "<br>");
+    // document.write("Result = " + result + "<br>"); //print the value 
+    return result;    //return the value of the function
 }
 
 // caling a function
-square();
+square(6);
+
+document.write(square(5))
+
+
+//add function 
+
+function addition(x,y){
+    var result = x + y;
+    document.write("sum = " + result + "<br>");
+}
+
+//sub function 
+
+function substraction(x,y){
+    var result = x - y;
+    document.write("sub = " + result + "<br>");
+}
+
+
+addition(10,20);
+addition(570,430);
+substraction(30, 20);
