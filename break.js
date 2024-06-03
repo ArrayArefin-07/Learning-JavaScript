8fae7fa88edbd61f98d4c7f7d3d92d16520c8e1f
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-06-03 12:33:11 +0600

    adding break and continue

---
+++ break.js
@@ -0,0 +1,22 @@
+//usecase of break
+
+for(i=1; i<=100; i++){
+    if (i==10) {
+        break;
+    }
+    document.write(" " + i);
+}
+
+document.write("End");
+
+
+//usecase of continue
+
+for(i=1; i<=100; i++){
+    if (i==10) {
+        continue;
+    }
+    document.write(" " + i);
+}
+
+document.write("End");
