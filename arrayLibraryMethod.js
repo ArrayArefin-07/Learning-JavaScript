81fe478f5e6fae6269568e748e7dac7b7ded4ff7
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-06-04 16:23:44 +0600

    Adding shift,Unshift and splice method

---
+++ arrayLibraryMethod.js
@@ -0,0 +1,23 @@
+
+//shift method
+
+
+var names = ["Arefin", "Anis", "wakil", ]
+
+console.log(names);
+
+// shift is opposit of pop
+
+names.shift();
+console.log(names);
+
+// Unshift opossit of push
+names.unshift("Mobasher");
+console.log(names);
+
+
+//Splice Method ---------
+
+// adding element
+names.splice(2,0,"Karim","Rahim");
+console.log(names);
