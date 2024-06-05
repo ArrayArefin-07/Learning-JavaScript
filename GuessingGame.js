b0492fa2862b87b394c06e9af9eb7a68b2e5a99d
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-06-05 16:20:08 +0600

    GuessingGame file added

---
+++ GuessingGame.js
@@ -0,0 +1,11 @@
+// Guesing Game 
+// Guess a number from 1 to 5 
+// Generate a random number between 1 to 5 
+// if the guess number matches random number ; then show message won else lost
+// Run the game for 5 times 
+// show the number of won and losts
+
+
+var guessNumber = parseInt(prompt("Enter a number from 1 to 5 : "));
+
+var randomNumber 
