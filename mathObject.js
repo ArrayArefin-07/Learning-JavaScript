2be6eee4126b5103b9e2fcaaa35fae9976fb48be
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-06-05 16:04:52 +0600

    learn some math function

--- mathObject.js
+++ mathObject.js
@@ -1,2 +1,25 @@
+// square root function
 var mt = Math.sqrt(25);
 console.log(mt);
+
+// Absolute function
+var mabs = Math.abs(-5);
+console.log(mabs);
+
+
+//sin x value
+var msin = Math.sin(2);
+console.log(msin);
+
+// Power function
+
+var mpow = Math.pow(2,4);
+console.log(mpow);
+
+
+var num1 = parseInt(prompt("Enter first number : "));
+var num2 = parseInt(prompt("Enter second number : "));
+
+var maximum = Math.max(num1,num2);
+console.log(maximum);
+
