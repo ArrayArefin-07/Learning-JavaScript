8bc804ae3673824a662db853248b3438952fc63a
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-06-06 02:27:54 +0600

    learn querySelectorAll method

--- querySelector.js
+++ querySelector.js
@@ -9,3 +9,16 @@ document.querySelector("#pid").innerHTML = "This is changed";
 
 //using class
 document.querySelector(".pidc").innerHTML = "This is changed by class";
+
+
+document.querySelector("a").innerHTML = "new text";
+document.querySelector("li a").innerHTML = "new text";
+
+document.querySelector("div a").innerHTML = "new text";
+
+document.querySelector(".my-div a").innerHTML = "new text using class";
+
+
+//querySelector
+
+document.querySelectorAll("p")[2].innerHTML = "new para";
