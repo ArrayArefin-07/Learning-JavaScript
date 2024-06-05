69acc7f1e592cac6c024404d9f737236926eef95
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-06-06 02:14:45 +0600

    learn query selector using id and class

--- querySelector.js
+++ querySelector.js
@@ -4,3 +4,8 @@
 
 // querySelector()
 
+// using id
+document.querySelector("#pid").innerHTML = "This is changed";
+
+//using class
+document.querySelector(".pidc").innerHTML = "This is changed by class";
