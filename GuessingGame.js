bd85208bc0abbe5e3e0a248f913bab3ba852bd78
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-06-05 16:35:33 +0600

    Make a gussing game it take user input and show won lost result

--- GuessingGame.js
+++ GuessingGame.js
@@ -6,6 +6,25 @@
 // show the number of won and losts
 
 
-var guessNumber = parseInt(prompt("Enter a number from 1 to 5 : "));
+var numOfWon = 0;
+var numOfLost = 0;
+for (var i = 1; i<5; i++){
 
-var randomNumber 
+    var guessNumber = parseInt(prompt("Enter a number from 1 to 5 : "));
+
+    var randomNumber = Math.floor(Math.random()*5) + 1;
+
+    if(guessNumber == randomNumber){
+        console.log("You have Won");
+        numOfWon++;
+    } 
+    else{
+        console.log("You have lost. RAndom number was " + randomNumber);
+        numOfLost++;
+    }
+}
+
+
+document.write("Total Number of won = " + numOfWon + "<br>");
+
+document.write("Total Number of Lost = " + numOfLost + "<br>");
