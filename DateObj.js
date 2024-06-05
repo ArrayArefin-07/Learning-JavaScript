030083c80a27efc5b8bc493958f5fac2c5da85dc
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-06-05 17:16:47 +0600

    adding some date related function

--- DateObj.js
+++ DateObj.js
@@ -4,3 +4,19 @@ console.log(date);
 
 var year = date.getFullYear();
 console.log(year);
+
+
+var month = date.getMonth();
+console.log(month);
+
+var currentDate = date.getDate();
+console.log(currentDate);
+
+var currentDay = date.getDay();
+console.log(currentDay);
+
+var currentHour = date.getHours();
+console.log(currentHour);
+
+var currentMinutes = date.getMinutes();
+console.log(currentMinutes);
