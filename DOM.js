f74a3472f9d27aee8ccbd971cd96458e9ed2cfdc
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-06-05 20:25:44 +0600

    adding Dom getELementById method

---
+++ DOM.js
@@ -0,0 +1,11 @@
+// getElementByID method
+
+document.getElementById("heading1").innerHTML = "Hello";
+
+document.getElementById("para").innerHTML = "Bye";
+
+document.getElementById("heading2").innerHTML = "Good Bye";
+
+var myheading2 = document.getElementById("heading2");
+
+myheading2.innerHTML = "Goodbye goodbye";
