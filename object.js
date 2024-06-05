06371f06c4132ada1d71091f3c78e91b3575673e
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-06-05 12:12:44 +0600

    Adding Object file and create a object

---
+++ object.js
@@ -0,0 +1,18 @@
+// student Details
+
+var name ="Mobasher Arefin";
+var age = 24;
+var cgpa = 4.00;
+
+var lang = ["bengali", "Hindi", "English"];
+
+// creating object 
+
+var student1 = {
+    name : "Arefin",
+    age : 24,
+    cgpa : 4.00,
+    lang : ["bengali", "Hindi", "English"]
+
+}
+console.log(student1.name);
