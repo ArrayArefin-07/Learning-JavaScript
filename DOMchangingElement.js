c9bee87f29c684bab247d6f9387a6d4a4ec2a0e1
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-06-07 20:41:35 +0600

    add Creating html element using js

--- DOMchangingElement.js
+++ DOMchangingElement.js
@@ -1,7 +1,13 @@
 var myvar = document.getElementsByTagName("h1")[0];
 myvar.innerHTML = "say hello";
 
+
 //creating html element  
 
+var heading3 = document.createElement("h1");
+var text = document.createTextNode("This is heading");
 
-var = heading3 = document
+heading3.appendChild(text)
+
+var myDiv = document.getElementById("my-div");
+myDiv.appendChild(heading3);
