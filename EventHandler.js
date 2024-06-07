97143e71850a69f6378a7cd400fd2435cdc5c374
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-06-07 18:15:52 +0600

    add images on onclick event

--- EventHandler.js
+++ EventHandler.js
@@ -10,3 +10,26 @@ function myMessage2(){
 function myMessage3(){
     alert("I am Paragraph")
 }
+
+var myVar = document.querySelector("#para");
+
+function myMessage() {
+
+    myVar.innerHTML = "You Have clicked on button1";
+}
+
+function myMessage2(){
+
+    myVar.innerHTML = "You Have clicked on button2";
+}
+
+//show Images
+
+var myImg = document.querySelector("#myImage");
+function myPicture1() {
+    myImg.src =  "images/my .jpg";
+}
+
+function myPicture2() {
+    myImg.src =  "images/my2.jpg";
+}
