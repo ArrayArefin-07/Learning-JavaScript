a4e39332390fc3bf1ff4702ff449d8db9a849908
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-06-08 16:20:50 +0600

    add for loop for simplyfing the code

--- eventListenerMultiple.js
+++ eventListenerMultiple.js
@@ -1,22 +1,34 @@
 
 
-// add Listener onn button 1
+// add Listener on button 1
 document.querySelectorAll(".myButton")[0].addEventListener("click", function(){
 
     var text = this.innerHTML; 
     document.querySelector("h1").innerHTML = text + " is clicked";
 });
 
-// add Listener onn button 2
+// add Listener on button 2
 document.querySelectorAll(".myButton")[1].addEventListener("click", function(){
 
     var text = this.innerHTML; 
     document.querySelector("h1").innerHTML = text + " is clicked";
 });
 
-// add Listener onn button 3
+// add Listener on button 3
 document.querySelectorAll(".myButton")[2].addEventListener("click", function(){
 
     var text = this.innerHTML; 
     document.querySelector("h1").innerHTML = text + " is clicked";
 });
+
+
+// use loop for simplifying the code
+var len = document.querySelectorAll(".myButton").length;
+
+for (var i =0; i <3; i++) {
+    document.querySelectorAll(".myButton")[i].addEventListener("click", function(){
+
+        var text = this.innerHTML; 
+        document.querySelector("h1").innerHTML = text + " is clicked";
+    });
+}
