e9fe953c2f6fd2f2f553d15eab0731efa314cfc8
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-06-08 14:45:50 +0600

    add mouseover & mouseout event Listener

--- eventListener.js
+++ eventListener.js
@@ -16,3 +16,14 @@ document.querySelector("button").addEventListener("click",function () {
 });
 
 
+var myVar = document.querySelector("h1");
+
+myVar.addEventListener("mouseover", function(){
+
+    myVar.classList.add("my-style");
+});
+
+myVar.addEventListener("mouseout", function(){
+
+    myVar.classList.remove("my-style");
+});
