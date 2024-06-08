2287602b47f7e574c514bba88b096333db87679b
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-06-08 11:44:23 +0600

    update Dom code

--- DOMchangingElement.js
+++ DOMchangingElement.js
@@ -5,7 +5,7 @@ myvar.innerHTML = "say hello";
 //creating html element  
 
 var heading3 = document.createElement("h1");
-var text = document.createTextNode("This is heading");
+var text = document.createTextNode("This is heading 3");
 
 heading3.appendChild(text)
 
@@ -17,3 +17,11 @@ myDiv.appendChild(heading3);
 var heading2 = document.getElementsByTagName("h1")[1];
 
 myDiv.removeChild(heading2);
+
+// add element in up
+
+var heading0 = document.createElement("h1");
+var text0 = document.createTextNode("This is heading 0");
+heading0.appendChild(text0);
+var heading1 = document.getElementsByTagName("h1")[0];
+myDiv.insertBefore(heading0, heading1);
