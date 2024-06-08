30b91318b44a164603ac52fc307084b4eebd2773
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-06-08 12:37:31 +0600

    changing html using JS insted of css

---
+++ ChangingCss.js
@@ -0,0 +1,9 @@
+
+function addStyle() {
+
+    var myVar = document.querySelector("#paraId");
+    myVar.style.color = "red";
+    myVar.style.fontSize  = "3rem";
+    myVar.style.fontWeight ="bold";
+    myVar.style.fontStyle = "italic";
+}
