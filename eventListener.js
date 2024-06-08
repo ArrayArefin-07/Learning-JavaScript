1d23ffd84698b2523ce7e8731e37745c80f8abda
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-06-08 14:38:00 +0600

    add anonymous function

--- eventListener.js
+++ eventListener.js
@@ -1,7 +1,18 @@
 
+// using event Listener 
 document.querySelector("button").addEventListener("click",myFunction);
 
-
+// define the function
 function myFunction() {
     alert("Hello");
 }
+
+
+
+// using anonymous function
+
+document.querySelector("button").addEventListener("click",function () {
+    alert("Hello");
+});
+
+
