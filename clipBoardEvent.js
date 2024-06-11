716b690fad412998ab2d9a248ddb6dd20ad1ced9
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-06-12 00:44:09 +0600

    add copy,cut, paste event

--- clipBoardEvent.js
+++ clipBoardEvent.js
@@ -2,3 +2,22 @@
 // oncopy
 //oncute
 // onpaste
+
+
+const input =  document.querySelector('input');
+const p =  document.querySelector('p');
+
+input.addEventListener('copy', function() {
+    p.innerText = 'you have copied';
+    // console.log('you have copied');
+});
+
+input.addEventListener('cut', function() {
+    p.innerText = 'you have cut';
+    // console.log('you have cut');
+});
+
+input.addEventListener('paste', function() {
+    p.innerText = 'you have pasted';
+    // console.log('you have pasted');
+});
