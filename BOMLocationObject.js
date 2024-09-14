6911c9f642fd5dda886f688b0adbf5737715c915
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-09-14 15:44:01 +0600

    Try to learning BOM location object

---
+++ BOMLocationObject.js
@@ -0,0 +1,23 @@
+// BOM (Browser Object Model)
+// window Object
+// location Object
+
+console.clear();
+// console.log(window.location);
+
+
+//href 
+console.log(location.href);
+
+//protocol
+
+console.log(location.protocol);
+
+//hostname 
+console.log(location.hostname);
+
+//port 
+console.log(location.port);
+
+//pathname
+console.log(location.pathname);
