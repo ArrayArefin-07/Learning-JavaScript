73e14132178c2b01bb8fa017f73364434cf46d28
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-09-14 16:02:36 +0600

    Learned BOM location object

--- BOMLocationObject.js
+++ BOMLocationObject.js
@@ -5,19 +5,42 @@
 console.clear();
 // console.log(window.location);
 
+// //href
+// console.log(location.href);
 
-//href 
-console.log(location.href);
+// //protocol
 
-//protocol
+// console.log(location.protocol);
 
-console.log(location.protocol);
+// //hostname
+// console.log(location.hostname);
 
-//hostname 
-console.log(location.hostname);
+// //port
+// console.log(location.port);
 
-//port 
-console.log(location.port);
+// //pathname
+// console.log(location.pathname);
 
-//pathname
-console.log(location.pathname);
+var locationDiv = document.querySelector(".location-div");
+// console.log(locationDiv);
+
+var p1 = locationDiv.children[0];
+// console.log(p1);
+p1.textContent = location.href;
+
+var p2 = locationDiv.children[1];
+p2.textContent = location.protocol;
+
+var p3 = locationDiv.children[2];
+p3.textContent = location.hostname;
+
+var p4 = locationDiv.children[3];
+p4.textContent = location.port;
+
+var p5 = locationDiv.children[4];
+p5.textContent = location.pathname;
+
+const visitButton = document.getElementById("visit-button");
+visitButton.addEventListener("click", function () {
+  location.assign("https://mobasherarefin.blogspot.com/")
+});
