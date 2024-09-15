32181ebd3b42433d64afcee1a0ac580917574d7e
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-09-16 03:30:05 +0600

    BOM Timing Event Added

---
+++ BOMTimingEvent.js
@@ -0,0 +1,18 @@
+// JS BOM 
+// JS Timing event methods 
+// setTimeout(), setInterval()
+
+
+// Uses of setTimeout function
+
+// setTimeout(() => {
+//   console.log("Hello, World!");
+// }, 5000);
+
+
+
+// setTimeout(display, 2000);
+
+// function display() {
+//   console.log("display function");
+// }
