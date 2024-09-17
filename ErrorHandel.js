094455606e5ebf79fe30828c094b1c3652e39928
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-09-18 01:04:20 +0600

    Start learning Error hendling

---
+++ ErrorHandel.js
@@ -0,0 +1,11 @@
+// Error handling -> try, catch, finally, throw 
+// try ... catch handle run time errors (program wich is )
+// Error object 
+//The finally statement -> execute code, after try and catch
+
+
+try{
+
+}catch() {
+  
+}
