b1c2c113b25917935d680ab7d65015a88f5755ab
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-09-19 03:44:54 +0600

    Learned try catch throw

--- ErrorHandel.js
+++ ErrorHandel.js
@@ -29,18 +29,20 @@
 document.querySelector("#checkButton").addEventListener("click", function(){
   // alert("Button is clicked");
   var num = document.querySelector("#numTextfield").value;
-  console.log(num); 
+  // console.log(num); 
   
 
   try {
-    if (num < 5 ) {
+    if (num < 5 ) 
+    {
       throw "input is too low"
-    } else if(num > 10){
+    } else if(num > 10)
+    {
       throw "input is too high"
     }
 
 
-  } catch (err) {
+  }catch (err) {
     console.log(err);
   }
 })
