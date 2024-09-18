f3bdf3299081be0ada3f224e2e778aa0a7bc49ca
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-09-19 03:32:10 +0600

    Added try-catch-throw

--- ErrorHandel.js
+++ ErrorHandel.js
@@ -30,4 +30,17 @@ document.querySelector("#checkButton").addEventListener("click", function(){
   // alert("Button is clicked");
   var num = document.querySelector("#numTextfield").value;
   console.log(num); 
+  
+
+  try {
+    if (num < 5 ) {
+      throw "input is too low"
+    } else if(num > 10){
+      throw "input is too high"
+    }
+
+
+  } catch (err) {
+    console.log(err);
+  }
 })
