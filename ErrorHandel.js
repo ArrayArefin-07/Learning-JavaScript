8636d632b9873f2f16a11ce77161c33a9f0be616
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-09-19 03:14:45 +0600

    error handle show alert button

--- ErrorHandel.js
+++ ErrorHandel.js
@@ -4,20 +4,30 @@
 //The finally statement -> execute code, after try and catch
 
 
-try{
+// try{
 
-  // code test
-  alert("Hi everyone");
+//   // code test
+//   alert("Hi everyone");
 
-  alert(x)
+//   alert(x)
   
 
-}catch(err){
-  //handle error
-  alert("Inside catch Block");
-  console.log(err);
-  console.log(err.name);
-  console.log(err.message);
-}finally{
-  alert("Bye Everyone");
-}
+// }catch(err){
+//   //handle error
+//   alert("Inside catch Block");
+//   console.log(err);
+//   console.log(err.name);
+//   console.log(err.message);
+// }finally{
+//   alert("Bye Everyone");
+// }
+
+
+//handle error using  throw keyword
+// the throw statement -> create custom errors
+
+document.querySelector("#checkButton").addEventListener("click", function(){
+  // alert("Button is clicked");
+  var num = document.querySelector("#numTextfield").value;
+  console.log(num); 
+})
