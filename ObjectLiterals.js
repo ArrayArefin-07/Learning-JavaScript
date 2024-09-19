63592fd3f63969386d768205bbea36615967b5ed
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-09-20 02:45:26 +0600

    Added ObjectLiterals file
    and added a function with object

---
+++ ObjectLiterals.js
@@ -0,0 +1,15 @@
+//Object Literals
+
+//Object property Initilizer Short Hand method
+
+function studentInfo1(name, age) {
+  return {
+    // name : name,
+    // age : age
+    //Use case in ES6 
+    name, 
+    age
+   }
+}
+
+console.log(studentInfo1("Mobasher Arefin", 24));
