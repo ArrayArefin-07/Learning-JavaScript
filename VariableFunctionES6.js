3a41fb0bcb468c64be8e6586c29ad66751a91b4c
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-09-19 15:46:17 +0600

    try to learned ES6 Variable

---
+++ VariableFunctionES6.js
@@ -0,0 +1,23 @@
+//using var keyword
+
+// var x = 6;
+// if (true) {
+//   var x = 5;
+// }
+// console.log(x)
+
+// using let keyword 
+
+// let x = 6;
+// if (true) {
+//   let x = 5;
+// }
+// console.log(x)
+
+//let using loop
+
+let x = 11;
+for (let x = 1; x <= 10; x++){
+  // console.log(x);
+}
+console.log(x)
