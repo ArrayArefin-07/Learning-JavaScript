9fadcee1106fc37388b42687e8d5b05cdf920f4f
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-09-20 02:51:47 +0600

    learned Object Literals
    Learned Concise method

--- ObjectLiterals.js
+++ ObjectLiterals.js
@@ -13,3 +13,17 @@ function studentInfo1(name, age) {
 }
 
 console.log(studentInfo1("Mobasher Arefin", 24));
+
+//concise methods 
+let message = {
+  // body : function(){
+  //   return `Hi, I am Object Function`
+  // }
+  //using concise method 
+
+  body(){
+    return `Hi, I am Object Function`
+  }
+}
+
+console.log(message.body())
