ab2c61238b9a17d9a2791be7691612c3d94f67f7
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-09-19 15:59:41 +0600

    Learned Es6 function

--- VariableFunctionES6.js
+++ VariableFunctionES6.js
@@ -37,12 +37,19 @@ console.log(message);
 
 //Regular Function in js
 
-function add(x,y) {
-  var sum = x + y;
-  console.log(sum);
-}
+// function add(x,y) {
+//   var sum = x + y;
+//   console.log(sum);
+// }
 
-add(20,30);
+// add(20,30);
 
 
 //Function in ES6
+
+const add = (x,y) => {
+  let sum = x + y;
+  console.log(sum);
+}
+
+add(20,40);
