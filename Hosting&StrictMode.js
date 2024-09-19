908bb12962655a704b3337c359ee8385b19fabbe
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-09-19 16:28:04 +0600

    learned Hosting mode in JS

---
+++ Hosting&StrictMode.js
@@ -0,0 +1,15 @@
+//Hosting and strict mode 
+
+x = 20;
+console.log(x);
+// var x;
+// let x;
+// const x;  
+if (true){
+  y = 10;
+  console.log(`y = ${y}`)
+  var y;
+}
+
+console.log(`x = ${x}`);
+var x;
