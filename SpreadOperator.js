98bb384f8129fdb3b63ef1ae8421011890bb4194
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-09-20 02:18:24 +0600

    leaned some basic of spread operator

---
+++ SpreadOperator.js
@@ -0,0 +1,20 @@
+
+function addNumbers(x, y, z){
+  return x + y + z;
+}
+
+let numbers = [1,2,3]
+console.log(addNumbers(numbers[0], numbers[1], numbers[2]))
+//Using spread Operator
+console.log(addNumbers(...numbers));
+
+let numbers1 = [...numbers, 5, 6];
+console.log(numbers1);
+
+let numbers3 = [4, 5, 6]
+
+let totalNumbers = numbers.concat(numbers3);
+console.log(totalNumbers);
+//concat using spread operator 
+let TotalNumbers = [...numbers, ...numbers3]
+console.log(TotalNumbers)
