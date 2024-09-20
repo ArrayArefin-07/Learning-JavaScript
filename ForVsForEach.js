5cdec6e143e67c54b6ed3b862db8b93275621d65
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-09-20 19:50:08 +0600

    Learned ForEach loop

--- ForVsForEach.js
+++ ForVsForEach.js
@@ -7,3 +7,25 @@ for(var x=0; x<numbers.length; x++ ){
   console.log(numbers[x]);
 }
 
+//uses of forEach Loop
+numbers.forEach(myFunction)
+function myFunction(x){
+  console.log(x);
+}
+
+
+numbers.forEach(function(x){
+  console.log(x);
+})
+
+var squareNumbers = [];
+numbers.forEach(function(x){
+  squareNumbers.push(x*x);
+})
+console.log(squareNumbers);
+
+//adding 5 everytime with numbers
+numbers.forEach(function(x,index,arr){
+  arr[index] = x+5;
+})
+console.log(numbers);
