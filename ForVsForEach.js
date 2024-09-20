cdd21e6e3ef284c907560cc03c2387628a35c48c
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-09-21 02:29:18 +0600

    learned Filter function

--- ForVsForEach.js
+++ ForVsForEach.js
@@ -32,6 +32,8 @@ console.log(numbers);
 
 
 // map, filter function 
+
+//uses of map function 
 var num = [2,3,4,5]
 var squreNum = num.map(function(x){
   // squreNum.push(x*x);
@@ -39,5 +41,12 @@ var squreNum = num.map(function(x){
   return x * x;
   
 })
-console.log("Map "+squreNum);
+console.log("Map " + squreNum);
  
+
+// Uses of filter function 
+var num1 = [9,22,31,4,5,35,26,78,77]
+var newNumbers = num1.filter(function(x){
+  return x>10;
+})
+console.log("Filter " + newNumbers);
