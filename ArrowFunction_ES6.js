0c4f5742b9edd3d8370a19b85fd360061d04aeef
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-09-22 00:29:48 +0600

    Learned Array Destructuring

---
+++ ArrowFunction_ES6.js
@@ -0,0 +1,4 @@
+ function display1(){
+      console.log("Hello World");
+    }
+   display1();
---
+++ DestructuringArray_&_Object.js
@@ -0,0 +1,13 @@
+//Array DEstructur
+
+let numbers = [10, 20, 30, 40, 50];
+let  [num1, num2, num3, num4, num5] = numbers
+let [numb1, numb2, ...z] = numbers
+console.log(num1);
+console.log(num2);
+console.log(z)
+
+//swap variables
+//Object destructure
+//Nested Object Destructure
+//Destructuring Function Parameters
