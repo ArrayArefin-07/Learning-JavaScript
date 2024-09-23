febf245b02c6d6cc3318e9fa13681aff9df5888a
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-09-23 12:15:37 +0600

    Learned String Method

---
+++ StringMethod.js
@@ -0,0 +1,12 @@
+// startsWith(searchString, position) -> check a string start with another string 
+
+const message = "Today is Friday";
+console.log(message.startsWith('Today', 0))
+
+//endsWith (searchString, lngth) -> check a string ends with another string
+console.log(message.endsWith('Today'))
+
+//includes(searchString, position) -> check if a string contains another string
+console.log(message.includes('Today'))
+
+//all thes methods are case sensitive 
