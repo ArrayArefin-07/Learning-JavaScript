70e0caf0c6f06e4259c9648a43871ecfb76898fd
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-09-23 13:58:19 +0600

    Learned ES6 Modules Basic

---
+++ ES6Modules&class.js
@@ -0,0 +1,7 @@
+import { text, setText } from './myModule.js';
+
+console.log(text);
+
+//useing function from module 
+setText("goodbye From ES6 ");
+console.log(text)
---
+++ myModule.js
@@ -0,0 +1,7 @@
+export let text = "Welcome to Module";
+
+//Create function in module
+
+export function setText(txt){
+  text = txt;
+}
