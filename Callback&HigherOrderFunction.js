8425f795737334486fe5442a5e01a275f85ae6ee
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-09-25 12:47:03 +0600

    learned basic of HigherOrder function
    learned Callback function basic

--- Callback&HigherOrderFunction.js
+++ Callback&HigherOrderFunction.js
@@ -9,3 +9,51 @@ function square(x) {
 // y(6)
 
 square(5);
+
+
+//higher Order Function 
+
+function higherOrderFunction(num, Callback){
+  Callback(num);
+}
+
+higherOrderFunction(6, square)
+
+
+
+const taskOne = (Callback) => {
+  console.log("Task-1");
+  Callback();
+};
+
+const taskTwo = (Callback) => {
+  setTimeout(() => {
+    console.log("Task-2. Data Loading");
+    Callback();
+  }, 3000);
+  
+};
+
+const taskThree = (Callback) => {
+  console.log("Task-3");
+  Callback();
+};
+
+const taskFour = (Callback) => {
+  console.log("Task-4");
+  Callback();
+};
+
+const taskFive = () => {
+  console.log("Task-5");
+};
+
+taskOne(function f1() {
+  taskTwo(function f2() {
+    taskThree(function f3() {
+      taskFour(function f4() {
+        taskFive();
+      })
+    });
+  });
+});
