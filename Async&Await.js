d9ddbaa5eceab713ccfffd30037ee230d480f8fd
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-09-26 16:35:59 +0600

    Added error handel in async await function

--- Async&Await.js
+++ Async&Await.js
@@ -6,15 +6,15 @@ const taskOne = () => {
 
 const taskTwo = () => {
   return new Promise((resolve, reject) => {
-    setTimeout(() =>{
+    setTimeout(() => {
       resolve("Task 2 is completed 2sec");
-    })
+    });
   });
 };
 
 const taskThree = () => {
   return new Promise((resolve, reject) => {
-    resolve("Task 3 is completed");
+    reject("Task 3 is not completed");
   });
 };
 
@@ -36,17 +36,21 @@ const taskFour = () => {
 // .catch((err) => console.log(err));
 
 async function callAllTasks() {
-  const t1 = await taskOne();
-  console.log(t1);
+  try {
+    const t1 = await taskOne();
+    console.log(t1);
 
-  const t2 = await taskTwo();
-  console.log(t2);
+    const t2 = await taskTwo();
+    console.log(t2);
 
-  const t3 = await taskThree();
-  console.log(t3);
+    const t3 = await taskThree();
+    console.log(t3);
 
-  const t4 = await taskFour();
-  console.log(t4);
+    const t4 = await taskFour();
+    console.log(t4);
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 callAllTasks();
