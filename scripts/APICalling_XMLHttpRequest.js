038ad045f7f70bebb3743a45a9b0dd0e5a7959cd
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-09-28 13:50:59 +0600

    Added file for learning fetch() API

--- scripts/APICalling_XMLHttpRequest.js
+++ scripts/APICalling_XMLHttpRequest.js
@@ -1,64 +1,67 @@
-console.clear();
+// 4 way to call api - XMLHttpRequest, fetch, axios, jquery
 
 //event - onload(), onerror()
 //property - response, responseText, responseType, responseURL, status, statusText
 // function - open(), send(), setRequestHeader()
 
 const makeRequest = (method, url, data) => {
-  const xhr = new XMLHttpRequest();
-  xhr.open(method, url);
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
 
-  xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.setRequestHeader("Content-Type", "application/json");
 
-  xhr.onload = () => {
-    let data = xhr.response;
-    // console.log(data)
-    console.log(JSON.parse(data));
-  };
+    xhr.onload = () => {
+      let data = xhr.response;
+      // console.log(data)
+      console.log(JSON.parse(data));
+    };
 
-  xhr.onerror = () => {
-    console.log("Error is here ");
-  };
-  xhr.send(JSON.stringify(data));
+    xhr.onerror = () => {
+      console.log("Error is here ");
+    };
+    xhr.send(JSON.stringify(data));
+  });
 };
 
 const getData = () => {
-  makeRequest("GET", "https://jsonplaceholder.typicode.com/posts");
+  makeRequest("GET", "https://jsonplaceholder.typicode.com/posts").then((res) =>
+    console.log(res)
+  );
 };
+getData();
 
-const sendData = () => {
-  makeRequest("POST", "https://jsonplaceholder.typicode.com/posts", {
-    title: "foo",
-    body: "bar",
-    userId: 1,
-  });
-};
+// const sendData = () => {
+//   makeRequest("POST", "https://jsonplaceholder.typicode.com/posts", {
+//     title: "foo",
+//     body: "bar",
+//     userId: 1,
+//   });
+// };
 
-//update data in api
-const updateData = () => {
-  makeRequest("PUT", "https://jsonplaceholder.typicode.com/posts/1", {
-    id: 1,
-    title: "fooNA",
-    body: "barMA",
-    userId: 1,
-  });
-};
+// //update data in api
+// const updateData = () => {
+//   makeRequest("PUT", "https://jsonplaceholder.typicode.com/posts/1", {
+//     id: 1,
+//     title: "fooNA",
+//     body: "barMA",
+//     userId: 1,
+//   });
+// };
 
-//Update single data
-const updateSingleData = () => {
-  makeRequest("PATCH", "https://jsonplaceholder.typicode.com/posts/1", {
-    title: "This is changed",
-  });
-};
+// //Update single data
+// const updateSingleData = () => {
+//   makeRequest("PATCH", "https://jsonplaceholder.typicode.com/posts/1", {
+//     title: "This is changed",
+//   });
+// };
 
-//Delete Data in API
-const deleteData = () => {
-  makeRequest("DELETE", "https://jsonplaceholder.typicode.com/posts/1", {});
-};
+// //Delete Data in API
+// const deleteData = () => {
+//   makeRequest("DELETE", "https://jsonplaceholder.typicode.com/posts/1", {});
+// };
 
-// sendData();
-// updateData();
-// updateSingleData();
-deleteData();
-
-//last watch 17:00 min
+// // sendData();
+// // updateData();
+// // updateSingleData();
+// deleteData();
---
+++ scripts/fetchAPI.js
@@ -0,0 +1,11 @@
+// 4 way to call api - XMLHttpRequest, fetch, axios, jquery
+
+//fetch() -has replaced XMLHttpRequest
+// fetch() -global method for making HTTP Request
+//2 way to call -the, async await
+
+// + fetch() is easy to use compare to XMLHttpReauest
+// + fetch() return a promise 
+// - returns a promise 
+// - returned promise can only handle networke error 
+// - dose not support all the older browser
