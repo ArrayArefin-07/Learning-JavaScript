0f1ecd8cfba410c825dd6245852664b64c3bd5ac
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-09-28 15:25:54 +0600

    Learned Creating a resources using fetch api

--- scripts/fetchAPI.js
+++ scripts/fetchAPI.js
@@ -11,13 +11,23 @@
 // - dose not support all the older browser
 
 console.clear();
-fetch("https://jsonplaceholder.typicode.com/posts")
-.then((res) => {
-  if (!res.ok) {
-    const message = `Eror: ${res.status}`;
-    throw new Error(message);
-  }
-  return res.json();
+fetch("https://jsonplaceholder.typicode.com/posts", {
+  method: "POST",
+  headers: {
+    "Content-type": "application/json; charset=UTF-8",
+  },
+  body: JSON.stringify({
+    title: "foo",
+    body: "bar",
+    userId: 1,
+  }),
 })
-.then((res) => console.log(res))
-.catch((err) => console.log(err));
+  .then((res) => {
+    if (!res.ok) {
+      const message = `Eror: ${res.status}`;
+      throw new Error(message);
+    }
+    return res.json();
+  })
+  .then((res) => console.log(res))
+  .catch((err) => console.log(err));
