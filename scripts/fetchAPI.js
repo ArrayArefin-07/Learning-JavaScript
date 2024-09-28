b4e915ea68b1fb6f7dd8dbbaa04e80dd332da174
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-09-28 15:06:47 +0600

    Learned getting a resource from fetch api
    Listing all resource using fetch api

--- scripts/fetchAPI.js
+++ scripts/fetchAPI.js
@@ -5,7 +5,19 @@
 //2 way to call -the, async await
 
 // + fetch() is easy to use compare to XMLHttpReauest
-// + fetch() return a promise 
-// - returns a promise 
-// - returned promise can only handle networke error 
+// + fetch() return a promise
+// - returns a promise
+// - returned promise can only handle networke error
 // - dose not support all the older browser
+
+console.clear();
+fetch("https://jsonplaceholder.typicode.com/posts")
+.then((res) => {
+  if (!res.ok) {
+    const message = `Eror: ${res.status}`;
+    throw new Error(message);
+  }
+  return res.json();
+})
+.then((res) => console.log(res))
+.catch((err) => console.log(err));
