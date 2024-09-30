37811c4b761b481d3b38c44170431025d8006584
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-09-30 17:43:30 +0600

    learned axios.delete method

--- scripts/axiosAPI.js
+++ scripts/axiosAPI.js
@@ -78,3 +78,10 @@ axios
 })
 .then((res) => console.log(res.data))
 .catch((err) => console.log(err));
+
+
+// axios.delete method
+axios
+.delete("https://jsonplaceholder.typicode.com/posts/1")
+.then((res) => console.log(res.data))
+.catch((err) => console.log(err));
