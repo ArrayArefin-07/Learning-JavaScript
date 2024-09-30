0c347f0c1e6a1aaeb8a5be96baa21d9cbe46b931
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-09-30 17:34:38 +0600

    axios.post  Method learned

--- scripts/axiosAPI.js
+++ scripts/axiosAPI.js
@@ -32,4 +32,20 @@ axios
 .then((res) => console.log(res.data))
 .catch((err) => console.log(err));
 
-// 
+
+
+// axios.post method 
+axios
+.post("https://jsonplaceholder.typicode.com/posts", {
+  method: 'POST',
+  body: JSON.stringify({
+    title: 'foo',
+    body: 'bar',
+    userId: 1,
+  }),
+ 
+})
+.then((res) => console.log(res.data))
+.catch((err) => console.log(err));
+
+
