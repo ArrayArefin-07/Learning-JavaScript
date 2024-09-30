ca6d845c6ea981c36545295e93a6f37e70fc6406
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-09-30 19:32:09 +0600

    Learned create data in axios api using async await

--- scripts/axiosAPI.js
+++ scripts/axiosAPI.js
@@ -1,15 +1,12 @@
 // 4 way to call api - XMLHttpRequest, fetch, axios, jquery
-// axios is a js library 
+// axios is a js library
 // it help to make request from browser (plain js/Vew/React/Angular) , node.js
-
-
 
 // + very easy to use
 // + it support all modern browser including IE
 // + it returns promise
 // + throws error brilliantly
 // + no need to set heder cause axios is intelligent
-
 
 //axios (config)
 // axios(url [, config])
@@ -20,87 +17,101 @@
 //axios.patch(url [, config])
 //axios.delete(url [, config])
 
-//axios return response object - data, status, statusText. header, config 
-
+//axios return response object - data, status, statusText. header, config
 
 console.clear();
 // console.log(window);
 
 // axios.get method
 axios
-.get("https://jsonplaceholder.typicode.com/posts")
-.then((res) => console.log(res.data))
-.catch((err) => console.log(err));
+  .get("https://jsonplaceholder.typicode.com/posts")
+  // .then((res) => console.log(res.data))
+  // .catch((err) => console.log(err));
 
-
-
-// axios.post method 
+// axios.post method
 axios
-.post("https://jsonplaceholder.typicode.com/posts", {
-  method: 'POST',
-  body: JSON.stringify({
-    title: 'foo',
-    body: 'bar',
-    userId: 1,
-  }),
- 
-})
-.then((res) => console.log(res.data))
-.catch((err) => console.log(err));
+  .post("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify({
+      title: "foo",
+      body: "bar",
+      userId: 1,
+    }),
+  })
+  // .then((res) => console.log(res.data))
+  // .catch((err) => console.log(err));
 
-// axios.put method 
+// axios.put method
 
 axios
-.put("https://jsonplaceholder.typicode.com/posts/1", {
-  method: 'PUT',
-  body: JSON.stringify({
-    id: 1,
-    title: 'fooMAAA',
-    body: 'barMAAAA',
-    userId: 1,
-  }),
- 
-})
-.then((res) => console.log(res.data))
-.catch((err) => console.log(err));
+  .put("https://jsonplaceholder.typicode.com/posts/1", {
+    method: "PUT",
+    body: JSON.stringify({
+      id: 1,
+      title: "fooMAAA",
+      body: "barMAAAA",
+      userId: 1,
+    }),
+  })
+  // .then((res) => console.log(res.data))
+  // .catch((err) => console.log(err));
 
 // axios.patch method
 
 axios
-.patch("https://jsonplaceholder.typicode.com/posts/1", {
-  method: 'PATCH',
-  body: JSON.stringify({
-    
-    body: 'barNaaaaaaaaaaa',
-    
-  }),
- 
-})
-.then((res) => console.log(res.data))
-.catch((err) => console.log(err));
-
+  .patch("https://jsonplaceholder.typicode.com/posts/1", {
+    method: "PATCH",
+    body: JSON.stringify({
+      body: "barNaaaaaaaaaaa",
+    }),
+  })
+  // .then((res) => console.log(res.data))
+  // .catch((err) => console.log(err));
 
 // axios.delete method
 axios
-.delete("https://jsonplaceholder.typicode.com/posts/1")
-.then((res) => console.log(res.data))
-.catch((err) => console.log(err));
-
+  .delete("https://jsonplaceholder.typicode.com/posts/1")
+  // .then((res) => console.log(res.data))
+  // .catch((err) => console.log(err));
 
 // use async await in axios method
 
-
-console.clear()
+console.clear();
 const makeRequest = async (config) => {
   return await axios(config);
 };
 
-const getData = () => {
-  makeRequest("https://jsonplaceholder.typicode.com/posts")
-  .then((res) => console.log(res))
-  .catch((err) => console.log(err));
+const createData = () => {
+  makeRequest({
+    url: "https://jsonplaceholder.typicode.com/posts",
+    method: "post",
+    data: JSON.stringify({
+      title: "Naafoo",
+      body: "bar",
+      userId: 1,
+    }),
+  })
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
 };
-getData();
+createData();
+
+//create data 
+
+// const createData = () => {
+//   makeRequest({
+//     url: "https://jsonplaceholder.typicode.com/posts",
+//     method: "post",
+//     data: JSON.stringify({
+//       title: "Naafoo",
+//       body: "bar",
+//       userId: 1,
+//     }),
+//   })
+//     .then((res) => console.log(res))
+//     .catch((err) => console.log(err));
+// };
+// createData();
 
 
 //get data
@@ -110,5 +121,3 @@ getData();
 //   .catch((err) => console.log(err));
 // };
 // getData();
-
-
