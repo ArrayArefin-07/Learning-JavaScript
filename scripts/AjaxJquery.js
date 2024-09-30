3bd449d7e40b24cedc6b77426e097c06d535fc0a
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-09-30 22:31:33 +0600

    learned basic of jquery POST method

--- scripts/AjaxJquery.js
+++ scripts/AjaxJquery.js
@@ -1,28 +1,40 @@
 // 4 way to call api - XMLHttpRequest, fetch, axios, jquery
 
 // add jquery library cdn
-//ajax -asynchronous javascript and xml 
+//ajax -asynchronous javascript and xml
 
 console.clear();
 // console.log(window);
 
-
-const makeRequest = async (url, method) => {
-  try{
+const makeRequest = async (url, method, data) => {
+  try {
     const result = await $.ajax({
       url: url,
       method: method,
+      data: data
     });
     // console.log(result);
     return result;
   } catch (err) {
     console.log(err);
   }
-}
-
-const getData = () => {
-  makeRequest('https://jsonplaceholder.typicode.com/posts/1', 'GET')
-  .then((res) => console.log(res))
 };
 
-getData();
+//Create data using jquery post method
+const createData = () => {
+  makeRequest("https://jsonplaceholder.typicode.com/posts", "POST", {
+    title: "foo",
+    body: "bar",
+    userId: 1,
+  }).then((res) => console.log(res));
+};
+
+createData();
+
+// //get data using jquery
+// const getData = () => {
+//   makeRequest('https://jsonplaceholder.typicode.com/posts/1', 'GET')
+//   .then((res) => console.log(res))
+// };
+
+// getData();
