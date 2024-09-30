f6e94e93c8cf39a11680a745ec1d0aacff1c779f
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-09-30 17:08:52 +0600

    added axios api file

---
+++ scripts/axiosAPI.js
@@ -0,0 +1,11 @@
+// 4 way to call api - XMLHttpRequest, fetch, axios, jquery
+// axios is a js library 
+// it help to make request from browser (plain js/Vew/React/Angular) , node.js
+
+
+
+// + very easy to use
+// + it support all modern browser including IE
+// + it returns promise
+// + throws error brilliantly
+// + no need to set heder cause axios is intelligent
